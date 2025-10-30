4d35499f1a43ee16adb01000d6b2cec57cd483f2
mariagiamos <mariagiamos07@gmail.com>
2025-10-30 17:27:28 -0400

    Update untitled0.py

--- untitled0.py
+++ untitled0.py
@@ -9,10 +9,11 @@ print("lab 6")
 import pandas as pd
 wdi_wide=pd.read_csv("wdi_wide.csv")
 print(wdi_wide)
+import Seaborn as sns
 
 #3
 #step 1, show data size and data types
-wdi_wide.info()
+print(wdi_wide.info())
 #Empty Values=Total entries-Non null count
 #Physicians= 217-207=10
 #Population= 217-217=0
@@ -22,4 +23,56 @@ print(wdi_wide.nunique())
 
 #5
 print(wdi_wide.describe())
-#this output of this fuction describes 
+#this output of this fuction describes the statistics of the GNI and the high income economy.
+#it shows the count (number of non empty values), mean (average GNI and income), the standard deviation, the minimum value, 25% percentile , 50% (median), 75% percentile and the maximum value. 
+
+#6
+#creating a new column GNI per capita
+wdi_wide["GNI per capita"]=wdi_wide["GNI"]/wdi_wide["Population"]
+
+#rounding to the nearest cent
+print(wdi_wide["GNI per capita"].round())
+
+#7
+#a. how many contries are in each region?
+print(wdi_wide["Region"].value_counts())
+
+
+#b . how many high incomes are there?
+print(wdi_wide["High Income Economy"].value_counts())
+#150 where high income= 0, 67 where high income=1
+#africa=54,asia=50, europe=47, americas=46, oceania=19
+
+#8 where are the high income economies (per region)
+print(pd.crosstab(wdi_wide["High Income Economy"], wdi_wide["Region"]))
+
+#9
+
+#Part 4
+import matplotlib as plt
+sns.replot(wdi_wide)
+plt.xlabel("GNI per capita")
+plt.ylabel="Life expectency"
+print(plt.show())
+
+
+
+
+      
+      
+      
+      
+                  
+                  
+      
+
+
+
+
+
+
+
+
+
+
+
