2004bf69544e7823cbc1c8fbfced74a0de7a3978
mariagiamos <mariagiamos07@gmail.com>
2025-10-29 22:29:07 -0400

    #1 and #2
    Chose 3 questions to be answered  and loaded csv file and printed it

--- untitled0.py
+++ untitled0.py
@@ -1,2 +1,11 @@
 print("lab 6")
 
+#1
+#Are population size and healthcare resources balanced across the contries?
+#is there a correlation between internet usage and ,ale life expectancy?
+#do females or males have a higher life expenctancy in regions?
+
+#2
+import pandas as pd
+wdi_wide=pd.read_csv("wdi_wide.csv")
+print(wdi_wide)
